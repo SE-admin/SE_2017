12e40d177d3fbbf2f07e2cacfcad722d8fb09dc4
SE-admin <chun.eunjin@ssarc.re.kr>
2017-04-05 21:21:36 +0900

    충돌사항 수정 및 최종본 fix

--- Hello.py
+++ Hello.py
@@ -1,10 +1,8 @@
 print ("Hello world")
 print ("Tell me your world")
 print ("Tell me his world")
-<<<<<<< HEAD
+print ("Tell me her world")
 print ("Tell me our world")
 
-=======
-print ("Tell me her world")
->>>>>>> 252b5c773a7f7599a57a26b5ffc9093be4a93630
+
 
